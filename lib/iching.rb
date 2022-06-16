d5966580835ea8569f51028aca588012077eff5a
Mark Kolmar <mark@burningrome.com>
2022-06-16 12:34:38 -0500

    Add command line flag to use yarrow stalk oracle rather than 3 coins

--- lib/iching.rb
+++ lib/iching.rb
@@ -4,8 +4,8 @@ require_relative 'hexagram_map.rb'
 require_relative 'hexagram.rb'
 
 class Iching
-  def display
-    hex1 = Hexagram.new
+  def display(oracle_type: :coin)
+    hex1 = Hexagram.new(oracle_type: oracle_type)
     puts hex1.hex_symbol, hex1.hex_name
     return if (hex1.hex & [6, 9]).none?
 
